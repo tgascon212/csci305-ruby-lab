cf86638be1f504d4c3916a329101e718f6ef0865
Tanner Gascon <tanner.a.gascon@gmail.com>
2018-02-12 12:25:24 -0700

    Commiting working first part of ruby lab:

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -4,13 +4,14 @@
 #
 # CSCI 305 - Ruby Programming Lab
 #
-# <firstname> <lastname>
-# <email-address>
+# <Tanner> <Gascon>
+# <tanner.a.gascon@gmail.com>
 #
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "<Tanner> <Gascon>"
+$counter = 0
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
@@ -18,15 +19,44 @@ def process_file(file_name)
 
 	begin
 		IO.foreach(file_name) do |line|
-			# do something for each line
+			#send the line to the cleanup function to return a proper title
+			title = cleanup_title(line)
 		end
-
+		puts $counter
 		puts "Finished. Bigram model built.\n"
 	rescue
 		STDERR.puts "Could not open file"
 		exit 4
 	end
 end
+
+def cleanup_title(line)
+	# split the line on the value "<SEP>"
+	values = line.split("<SEP>")
+
+	# create a variable title and store the title of the song
+	title = values[3]
+
+	# Erase everything after a left parenthesis, bracket, curley brace, +, \, *, /, _, -, :, ', =, and following feat.
+	title = title.gsub(/(\(.*|\[.*|\{.*|\+.*|\\.*|\*.*|\/.*|_.*|-.*|:.*|'.*|=.*|(feat..*))/,"")
+
+	# Remove characters ?  ¿  !  ¡  .  ;  &  @  %  #  |
+	title = title.gsub(/(\?|¿|!|¡|\.|;|&|@|%|#|\|)/,"")
+
+	# Downcase the title
+	title = title.downcase
+
+	# Remove any non-ASCII song titles
+	if title.match(/^[\d\w\s']+$/)
+		#print the title and increment the counter
+		puts title
+		$counter=$counter+1
+	end
+
+	return title
+end
+
+
 
 # Executes the program
 def main_loop()
